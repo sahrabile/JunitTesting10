2ad3bc00e6fc43ad37eaed79cb71c03a9ddf86a1
Sahra <90868564+sahrabile@users.noreply.github.com>
2021-11-30 14:59:16 +0100

    första lektion tillsammans

---
+++ Testning/BackEndTest.java
@@ -0,0 +1,62 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BackEndTest {
+
+    @Test
+    void getWinner() {
+    }
+
+    @Test
+    void currentPlayerName() {
+    }
+
+    @Test
+    void randomStarter() {
+    }
+
+    @Test
+    void player1Score() {
+    }
+
+    @Test
+    void player2Score() {
+    }
+
+    @Test
+    void throwDice() {
+    }
+
+    @Test
+    void addScore() {
+    }
+
+    @Test
+    void startGame() {
+    }
+
+    @Test
+    void askToKeep() {
+    }
+
+    @Test
+    void getPlayer1() {
+    }
+
+    @Test
+    void getPlayer2() {
+    }
+
+    @Test
+    void getCurrentPlayer() {
+    }
+
+    @Test
+    void getDice() {
+    }
+
+    @Test
+    void gameOver() {
+    }
+}
--- src/BackEnd.java
+++ src/BackEnd.java
@@ -18,43 +18,71 @@ public class BackEnd {
 
     //Ska returnera en sträng: "And the Winner is " samt namnet på den spelare som har högst poäng
     public String getWinner(){
-        //TODO
-        return "Not implemented";
+        String player1Win = "And the Winner is " + player1;
+        String player2Win = "And the Winner is " + player2;
+        String gameOver = "No one win!";
+        if(p1Score >= 100){
+            return player1Win;
+        }else if (p2Score >= 100){
+            return player2Win;
+        }
+        return gameOver;
     }
 
     // Ska returnera namnet på den spelaren i tur utifrån värdet på currentplayer
-    public String currentPlayerName(){
-        //TODO
-        return "Not implemented";
-
+    public String currentPlayerName() {
+        if (currentPlayer == 2 && roundCounter >= 3) {
+            currentPlayer = 1;
+            return player1;
+        }
+        else {
+            currentPlayer = 2;
+            return player2;
+        }
     }
 
     //Sätter this.currentPlayer till 1 eller 2 baserat på slumpen.
     public void randomStarter(){
-        //TODO
+        this.currentPlayer = (int) (Math.random() * 2);
     }
 
     //Returnerar en sträng med spelare1:s namn följt av "Score: " samt spelare1:s poäng
     public String player1Score(){
-        //TODO
-        return "Not implemented";
+        String playersStats1 = player1 + " score " + p1Score;
+
+        return playersStats1;
 
     }
     //Returnerar en sträng med spelare2:s namn följt av "Score: " samt spelare2:s poäng
     public String player2Score(){
-        //TODO
-        return "Not implemented";
+        String playersStats2 = player2 + " score " + p2Score;
+
+        return playersStats2;
     }
 
     //Ger this.Dice ett värde mellan 1 och 20, samt ökar this.roundCounter med 1;
     public void throwDice(){
-        //TODO
+       this.dice = (int) (Math.random() * 20);
+       this.roundCounter++;
     }
     // Lägger till värdet på this.dice till den aktuelle spelaren poäng
     // Nollställer därefter this.dice och this.roundCounter
     //Kollar sedan om någon av spelarna har nått 100 poäng och om så är fallet sätts gameOver till true;
     public void addScore(){
-        //TODO
+        if(currentPlayer == 1){
+            p1Score  = this.dice + p1Score;
+        }
+        else{
+            p2Score  = this.dice + p2Score;
+        }
+
+        if(p1Score >= 100 || p2Score >= 100){
+            gameOver = true;
+
+        }
+        this.dice = 0;
+        this.roundCounter = 0;
+
     }
 
 //Inget ska behöva ändras eller testas koden härunder
